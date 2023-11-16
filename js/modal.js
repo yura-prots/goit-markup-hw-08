df8615062180f101b6cf4000ff7a48b90c23d427
yura-prots <yuraprots@gmail.com>
2023-11-16 09:45:44 +0200

    завершив адаптацію головної сторінки

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle('modal-open');
     refs.backdrop.classList.toggle('is-hidden');
   }
 })();
